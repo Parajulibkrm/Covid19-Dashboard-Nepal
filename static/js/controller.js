2207211b09ac1229e72a4ecd1544307b97acec85
Parajulibkrm <39263700+Parajulibkrm@users.noreply.github.com>
2020-04-26 18:07:13 +0545

    Updated dataset source

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,170 +1,172 @@
 d3.queue()
-  .defer(d3.json,"https://raw.githubusercontent.com/Parajulibkrm/new-nepal-geojson-and-topojson/master/topojson/Nepaltopo.json")
-  .defer(d3.csv,"https://raw.githubusercontent.com/Parajulibkrm/covid19-district-data-nepal/master/CoronaNepal.csv", (row)=>({
-    districtId: row.ID,
-    district: row.District,
-    data_date: new Date(row.data_Date),
-    recovered: +row.Recovered,
-    data_date_raw: row.data_Date,
-    Cases: +row.Cases,
-    Deaths: +row.Deaths,
-    Active: +row.Cases - +row.Deaths - +row.Recovered,
-    date_month: +(row.data_Date).substr(5,2),
-    date_day: +(row.data_Date).substr(8,2)
-  }))
-  .await((error,mapData,coronaData)=> {
-      if (error) throw error;
-    //   console.log(geoData)
+    .defer(d3.json, "https://raw.githubusercontent.com/Parajulibkrm/new-nepal-geojson-and-topojson/master/topojson/Nepaltopo.json")
+    .defer(d3.csv, "https://covid-dataset-by-bikram.herokuapp.com/CoronaNepal.csv", (row) => ({
+        districtId: row.ID,
+        district: row.District,
+        data_date: new Date(row.data_Date),
+        recovered: +row.Recovered,
+        data_date_raw: row.data_Date,
+        Cases: +row.Cases,
+        Deaths: +row.Deaths,
+        Active: +row.Cases - +row.Deaths - +row.Recovered,
+        date_month: +(row.data_Date).substr(5, 2),
+        date_day: +(row.data_Date).substr(8, 2)
+    }))
+    .await((error, mapData, coronaData) => {
+        if (error) throw error;
+        //   console.log(geoData)
 
-      let geoData = topojson.feature(mapData, mapData.objects.codebeautify).features;
+        let geoData = topojson.feature(mapData, mapData.objects.codebeautify).features;
 
-      let minDate = d3.min(coronaData,d => new Date(d.data_date))
-      let maxDate = d3.max(coronaData,d => new Date(d.data_date))
-      var diffSec = maxDate.getTime() - minDate.getTime(); 
-      var diffDays = diffSec / (1000 * 3600 * 24);
+        let minDate = d3.min(coronaData, d => new Date(d.data_date))
+        let maxDate = d3.max(coronaData, d => new Date(d.data_date))
+        var diffSec = maxDate.getTime() - minDate.getTime();
+        var diffDays = diffSec / (1000 * 3600 * 24);
 
 
-      let daySelector = d3.select('#day-select')
-                          .attr("min",0)
-                          .attr("max",diffDays)
-    daySelector.attr("value",diffDays)
-    let selectedDay = new Date(minDate.getTime()+daySelector.property("value")*86400000)
-    let selectedDistrict = 27;
-    let lineGraph = document.getElementById('slct')
-    let optionSelect = d3.selectAll("input[name='selector']")
-    let option= optionSelect.property("value")
+        let daySelector = d3.select('#day-select')
+            .attr("min", 0)
+            .attr("max", diffDays)
+        daySelector.attr("value", diffDays)
+        let selectedDay = new Date(minDate.getTime() + daySelector.property("value") * 86400000)
+        let selectedDistrict = 27;
+        let lineGraph = document.getElementById('slct')
+        let optionSelect = d3.selectAll("input[name='selector']")
+        let option = optionSelect.property("value")
 
-    
-    const dataOfDay = (coronaData,selectedDay) => {
-        let dataOftheDay = coronaData.filter(d => +d.data_date == +selectedDay)
-        return dataOftheDay;
-    }
-    const dataOfDistrict = (districtId) => {
-        let dataOfTheDistrict = coronaData.filter(d => +d.districtId == districtId )
-        return dataOfTheDistrict
-    }
 
-    console.log(geoData)
-    let latestTotal = d3.sum(dataOfDay(coronaData,maxDate),d => d.Cases)
-    let latestActive = d3.sum(dataOfDay(coronaData,maxDate), (d) => d.Active)
-    let latestDeath = d3.sum(dataOfDay(coronaData,maxDate), (d) => d.Deaths)
-    let latestRecovered = d3.sum(dataOfDay(coronaData,maxDate), (d) => d.recovered)
-    let totalNepalPiDomain = [latestActive,latestRecovered,latestDeath]
-    var playing = false;
-    
-    const setHeaderData= () => {
-        document.getElementById("tc").textContent= latestTotal;
-        document.getElementById("ta").textContent= latestActive;
-        document.getElementById("tr").textContent= latestRecovered;
-        document.getElementById("td").textContent= latestDeath;
-        // console.log(maxDate.toLocaleString('default', { month: 'long' })+' ' + coronaData.date_day)
-        document.getElementById("ld").textContent= maxDate.toLocaleString('default', { month: 'long' })+' '+maxDate.getDate();
-    }
-    const setLineGraphRange= () => {
-        var sel = document.getElementById('slct');
-        for (let index = 7; index < diffDays; index++) {
-            var opt = document.createElement('option');
-            opt.appendChild( document.createTextNode(index) );
-            if (index == 30 ) opt.selected = "true"
-            opt.value = index;
-            sel.appendChild(opt);
-            // console.log('done') 
+        const dataOfDay = (coronaData, selectedDay) => {
+            let dataOftheDay = coronaData.filter(d => +d.data_date == +selectedDay)
+            return dataOftheDay;
         }
-    }
-    const getProvinceSum = (num) =>{
-        var totalsum = 0;
-        geoData.forEach(district => {
-            if (district.properties.Province == num) {
-                let d = dataOfDay(dataOfDistrict(district.properties.districtid),maxDate);
-                totalsum += +d[0].Cases
+        const dataOfDistrict = (districtId) => {
+            let dataOfTheDistrict = coronaData.filter(d => +d.districtId == districtId)
+            return dataOfTheDistrict
+        }
+
+        console.log(geoData)
+        let latestTotal = d3.sum(dataOfDay(coronaData, maxDate), d => d.Cases)
+        let latestActive = d3.sum(dataOfDay(coronaData, maxDate), (d) => d.Active)
+        let latestDeath = d3.sum(dataOfDay(coronaData, maxDate), (d) => d.Deaths)
+        let latestRecovered = d3.sum(dataOfDay(coronaData, maxDate), (d) => d.recovered)
+        let totalNepalPiDomain = [latestActive, latestRecovered, latestDeath]
+        var playing = true;
+
+        const setHeaderData = () => {
+            document.getElementById("tc").textContent = latestTotal;
+            document.getElementById("ta").textContent = latestActive;
+            document.getElementById("tr").textContent = latestRecovered;
+            document.getElementById("td").textContent = latestDeath;
+            // console.log(maxDate.toLocaleString('default', { month: 'long' })+' ' + coronaData.date_day)
+            document.getElementById("ld").textContent = maxDate.toLocaleString('default', {
+                month: 'long'
+            }) + ' ' + maxDate.getDate();
+        }
+        const setLineGraphRange = () => {
+            var sel = document.getElementById('slct');
+            for (let index = 7; index < diffDays; index++) {
+                var opt = document.createElement('option');
+                opt.appendChild(document.createTextNode(index));
+                if (index == 30) opt.selected = "true"
+                opt.value = index;
+                sel.appendChild(opt);
+                // console.log('done') 
             }
-        });
-    return totalsum;
-    }
-    const fillTable = () => {
-        dataOfDay(coronaData,maxDate)
-        .sort((a,b)=> b.Cases- a.Cases)
-        .forEach((d)=> {
-                var table = document.getElementById("districtTable");
-                var newRow = table.insertRow(table.length);
-                var cell = newRow.insertCell(0);
-                var cell2 = newRow.insertCell(1);
-                var cell3 = newRow.insertCell(2);
-                cell.innerHTML = d.district;
-                console.log(d.Active)
-                cell2.innerHTML = d.Cases;
-                cell3.innerHTML = d.Active;
-        })
-    }
-    fillTable()
-    let totalProvincePiDomain = [getProvinceSum(1),getProvinceSum(2),getProvinceSum(3),getProvinceSum(4),getProvinceSum(5),getProvinceSum(6),getProvinceSum(7)]
-    drawProvincePi(totalProvincePiDomain);
-    console.log(totalNepalPiDomain)
-    drawNepalPi(totalNepalPiDomain)
-    drawDistrictPie(dataOfDay(dataOfDistrict(selectedDistrict),selectedDay));
-    drawMap(geoData);
-    colorMap(dataOfDay(coronaData,selectedDay),option);
-    setHeaderData();
-    setLineGraphRange();
-    let lineGraphRange = lineGraph.options[lineGraph.selectedIndex].value;
-    console.log(lineGraphRange)
-    drawLine(dataOfDistrict(selectedDistrict),lineGraphRange);
-
-    d3.select('#slct')
-      .on("change", d => {
-         lineGraphRange = document.getElementById("slct").value
-         window.myChart.destroy()
-        drawLine(dataOfDistrict(selectedDistrict),lineGraphRange);
-      })
-
-    daySelector.on("change", d => {
-        selectedDay = new Date(minDate.getTime()+d3.event.target.value*86400000)
-        d3.select("#DateOfData").remove();
-        colorMap(dataOfDay(coronaData,selectedDay),option);
-        window.myDoughnut.destroy()
-        drawDistrictPie(dataOfDay(dataOfDistrict(selectedDistrict),selectedDay));
-        // window.myDoughnut.update()
-    })
-    optionSelect.on("change",d => {
-        option = d3.event.target.value
-        d3.select("#DateOfData").remove();
-        colorMap(dataOfDay(coronaData,selectedDay),option);
-    })
-
-    d3.select('#playButton')
-      .on("click",() => {
-        if (playing) document.getElementById('playButton').innerHTML = "Pause"
-        if (!playing) document.getElementById('playButton').innerHTML = "Play"
-          playing = !playing;
-          if (playing) return;
-            window.setInterval(()=> {
-                if (+selectedDay == +maxDate) {
-                    document.getElementById('playButton').innerHTML = "Play"
-                    playing = true;
-                    return};
-                if (playing) return;
-                if (selectedDay !== maxDate){
-                    console.log(selectedDay)
-                    selectedDay = new Date(selectedDay.getTime() + 86400000);
-                    d3.select("#DateOfData").remove();
-                    colorMap(dataOfDay(coronaData,selectedDay),option);
-
-                    var diffSecs = selectedDay.getTime() - minDate.getTime(); 
-                    var diffDay = diffSecs / (1000 * 3600 * 24);
-                    document.getElementById('day-select').stepUp(1) 
+        }
+        const getProvinceSum = (num) => {
+            var totalsum = 0;
+            geoData.forEach(district => {
+                if (district.properties.Province == num) {
+                    let d = dataOfDay(dataOfDistrict(district.properties.districtid), maxDate);
+                    totalsum += +d[0].Cases
                 }
-            },300);
-      })
+            });
+            return totalsum;
+        }
+        const fillTable = () => {
+            dataOfDay(coronaData, maxDate)
+                .sort((a, b) => b.Cases - a.Cases)
+                .forEach((d) => {
+                    var table = document.getElementById("districtTable");
+                    var newRow = table.insertRow(table.length);
+                    var cell = newRow.insertCell(0);
+                    var cell2 = newRow.insertCell(1);
+                    var cell3 = newRow.insertCell(2);
+                    cell.innerHTML = d.district;
+                    console.log(d.Active)
+                    cell2.innerHTML = d.Cases;
+                    cell3.innerHTML = d.Active;
+                })
+        }
+        fillTable()
+        let totalProvincePiDomain = [getProvinceSum(1), getProvinceSum(2), getProvinceSum(3), getProvinceSum(4), getProvinceSum(5), getProvinceSum(6), getProvinceSum(7)]
+        drawProvincePi(totalProvincePiDomain);
+        console.log(totalNepalPiDomain)
+        drawNepalPi(totalNepalPiDomain)
+        drawDistrictPie(dataOfDay(dataOfDistrict(selectedDistrict), selectedDay));
+        drawMap(geoData);
+        colorMap(dataOfDay(coronaData, selectedDay), option);
+        setHeaderData();
+        setLineGraphRange();
+        let lineGraphRange = lineGraph.options[lineGraph.selectedIndex].value;
+        console.log(lineGraphRange)
+        drawLine(dataOfDistrict(selectedDistrict), lineGraphRange);
 
-    d3.selectAll('.district')
-      .on("click",(d)=> {
-          console.log("clicked")
-          selectedDistrict = d.properties.districtid
-          let filtData =dataOfDistrict(selectedDistrict)
-          window.myChart.destroy()
-          drawLine(filtData,lineGraphRange);
-          window.myDoughnut.destroy()
-          drawDistrictPie(dataOfDay(filtData,selectedDay));
-      })
-    }
-)
+        d3.select('#slct')
+            .on("change", d => {
+                lineGraphRange = document.getElementById("slct").value
+                window.myChart.destroy()
+                drawLine(dataOfDistrict(selectedDistrict), lineGraphRange);
+            })
+
+        daySelector.on("change", d => {
+            selectedDay = new Date(minDate.getTime() + d3.event.target.value * 86400000)
+            d3.select("#DateOfData").remove();
+            colorMap(dataOfDay(coronaData, selectedDay), option);
+            window.myDoughnut.destroy()
+            drawDistrictPie(dataOfDay(dataOfDistrict(selectedDistrict), selectedDay));
+            // window.myDoughnut.update()
+        })
+        optionSelect.on("change", d => {
+            option = d3.event.target.value
+            d3.select("#DateOfData").remove();
+            colorMap(dataOfDay(coronaData, selectedDay), option);
+        })
+
+        d3.select('#playButton')
+            .on("click", () => {
+                if (playing) document.getElementById('playButton').innerHTML = "◼"
+                if (!playing) document.getElementById('playButton').innerHTML = "‎▶"
+                playing = !playing;
+                if (playing) return;
+                window.setInterval(() => {
+                    if (+selectedDay == +maxDate) {
+                        document.getElementById('playButton').innerHTML = "◼"
+                        playing = true;
+                        return
+                    };
+                    if (playing) return;
+                    if (selectedDay !== maxDate) {
+                        console.log(selectedDay)
+                        selectedDay = new Date(selectedDay.getTime() + 86400000);
+                        d3.select("#DateOfData").remove();
+                        colorMap(dataOfDay(coronaData, selectedDay), option);
+
+                        var diffSecs = selectedDay.getTime() - minDate.getTime();
+                        var diffDay = diffSecs / (1000 * 3600 * 24);
+                        document.getElementById('day-select').stepUp(1)
+                    }
+                }, 300);
+            })
+
+        d3.selectAll('.district')
+            .on("click", (d) => {
+                console.log("clicked")
+                selectedDistrict = d.properties.districtid
+                let filtData = dataOfDistrict(selectedDistrict)
+                window.myChart.destroy()
+                drawLine(filtData, lineGraphRange);
+                window.myDoughnut.destroy()
+                drawDistrictPie(dataOfDay(filtData, selectedDay));
+            })
+    })
--- static/js/map.js
+++ static/js/map.js
@@ -1,124 +1,133 @@
 function drawMap(geoData) {
-    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    var margin = {
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10
+        },
         width = 960 - margin.left - margin.right,
         height = 640 - margin.top - margin.bottom;
-    
+
     var projection = d3.geoMercator()
-        .translate([300,150])
+        .translate([300, 150])
         .scale(3500)
-	    .center([83.985593872070313, 28.465876770019531]);
+        .center([83.985593872070313, 28.465876770019531]);
 
     var path = d3.geoPath()
         .projection(projection);
-    
+
     var svg = d3.select('.map')
-    .append('svg')
-    .attr("viewBox", `0 0 600 300`)
-    .append('g')
-    .call(d3.zoom().scaleExtent([1,3]).on("zoom", function () {
-        svg.attr("transform", d3.event.transform)
-     }))
+        .append('svg')
+        .attr("viewBox", `0 0 600 300`)
+        .append('g')
+        .call(d3.zoom().scaleExtent([1, 3]).on("zoom", function () {
+            svg.attr("transform", d3.event.transform)
+        }))
 
     var map = svg.selectAll('.districts')
-                 .data(geoData)
-                 .enter()
-                 .append("path")
-                 .attr("class","district")
-                 .attr("d",path)
-                 .attr("fill","#e3e3e3")                          
-                
+        .data(geoData)
+        .enter()
+        .append("path")
+        .attr("class", "district")
+        .attr("d", path)
+        .attr("fill", "#e3e3e3")
+
 }
-function getColor(data,option) {
+
+function getColor(data, option) {
     let range;
-    let dataMax = d3.max(data,d => d[option])
+    let dataMax = d3.max(data, d => d[option])
     let rangeIndex;
     if (dataMax < 8) {
         rangeIndex = dataMax;
     } else {
         rangeIndex = 9;
     }
-     
-    if(option == "Cases"){
+
+    if (option == "Cases") {
         range = colorbrewer.Blues[rangeIndex]
-    }
-    else if (option == "Deaths"){
+    } else if (option == "Deaths") {
         range = colorbrewer.Reds[rangeIndex]
-    }
-    else if (option =="recovered"){
+    } else if (option == "recovered") {
         range = colorbrewer.Greens[rangeIndex]
-    }
-    else if (option == "Active"){
+    } else if (option == "Active") {
         range = colorbrewer.Oranges[rangeIndex]
     }
     return d3.scaleQuantize()
-             .range(range)
-             .domain([0,d3.max(data, d => d[option])])
+        .range(range)
+        .domain([0, d3.max(data, d => d[option]) + 1])
 }
-function colorMap(dataOfDay,option) {
+
+function colorMap(dataOfDay, option) {
     // console.log(dataOfDay.filter(d => d.districtId == "71"))
     d3.selectAll('g.legendEntry').remove()
-    function colorthat(dataOfDay,did){
+
+    function colorthat(dataOfDay, did) {
         console.log(dataOfDay[0].districtId)
         // console.log(did)
         let data = dataOfDay.filter(d => (d.districtId == did))
         let data2 = data[0]
-        // if (data2[option] == 0){
-        //     return "#ffffff"
-        // }
-        return getColor(dataOfDay,option)(data2[option])        
+        if (data2[option] == 0) {
+            return "#ffffff"
+        }
+        return getColor(dataOfDay, option)(data2[option])
     }
 
     d3.selectAll('.district')
-      .transition()
-      .duration(300)
-      .ease(d3.easeQuadIn)
-      .attr("fill",d => {
-        //   console.log(d.properties.districtid)
-          return colorthat(dataOfDay,d.properties.districtid)
-    })
+        .transition()
+        .duration(300)
+        .ease(d3.easeQuadIn)
+        .attr("fill", d => {
+            //   console.log(d.properties.districtid)
+            return colorthat(dataOfDay, d.properties.districtid)
+        })
     var dateEntry = d3.select('svg')
-                    .data(['0'])
-                    .append('text')
-                    .attr("id","DateOfData")
-                    .attr("x",10)
-                    .attr("y",290)
-                    .text(dataOfDay[0].data_date.toLocaleString('default', { month: 'long' })+' ' + dataOfDay[0].date_day)
+        .data(['0'])
+        .append('text')
+        .attr("id", "DateOfData")
+        .attr("x", 10)
+        .attr("y", 290)
+        .text(dataOfDay[0].data_date.toLocaleString('default', {
+            month: 'long'
+        }) + ' ' + dataOfDay[0].date_day)
 
     var legend = d3.select('svg')
         .selectAll('g.legendEntry')
-		.data(getColor(dataOfDay,option).range())
-		.enter()
-		.append('g')
-		.attr("transform", "translate(" + 0 + ", " + 10 + ")")
-		.attr('class', 'legendEntry');
-	
-	legend
-		.append('rect')
-		.attr("x", 600 - 85)
-		.attr("y", function(d, i) {
-			return i * 10;
-		})
-		.attr("width", 20)
-		.attr("height", 10)
-		.style("stroke", "black")
-		.style("stroke-width", 0.1)
-		.style("fill", function(d){return d;}); 
-			//the data objects are the fill colors
-	
-	legend
-		.append('text')
-		.attr("x", 600 - 60) //leave 5 pixel space after the <rect>
-		.attr("y", function(d, i) {
-			return i * 10;
-		})
-		.attr("dy", "1em") //place text one line *below* the x,y point
-		.style("font-family","Helvetica")
-		.style("font-size",10)
-		.text(function(d) {
-			var r = getColor(dataOfDay,option).invertExtent(d);
+        .data(getColor(dataOfDay, option).range())
+        .enter()
+        .append('g')
+        .attr("transform", "translate(" + 0 + ", " + 10 + ")")
+        .attr('class', 'legendEntry');
+
+    legend
+        .append('rect')
+        .attr("x", 600 - 95)
+        .attr("y", function (d, i) {
+            return i * 10;
+        })
+        .attr("width", 20)
+        .attr("height", 10)
+        .style("stroke", "black")
+        .style("stroke-width", 0.1)
+        .style("fill", function (d) {
+            return d;
+        });
+    //the data objects are the fill colors
+
+    legend
+        .append('text')
+        .attr("x", 600 - 70) //leave 5 pixel space after the <rect>
+        .attr("y", function (d, i) {
+            return i * 10;
+        })
+        .attr("dy", "1em") //place text one line *below* the x,y point
+        .style("font-family", "Helvetica")
+        .style("font-size", 10)
+        .text(function (d) {
+            var r = getColor(dataOfDay, option).invertExtent(d);
             if (r[1]) {
-                return r[0].toFixed(1)+" to "+ r[1].toFixed(1)
+                return r[0].toFixed(0).padStart(2, "0") + " to " + r[1].toFixed(0).padStart(2, "0") + " Cases"
             }
             return "0.0 to 0.0"
-		});
+        });
 }
